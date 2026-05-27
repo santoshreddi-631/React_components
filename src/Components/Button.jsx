39b72b57a750078bdcc84d07c8f32d436b8e6ef1
santoshreddi-631 <reddisantosh772@gmail.com>
2026-05-27 21:44:49 +0530

    Button component

---
+++ src/Components/Button.jsx
@@ -0,0 +1,12 @@
+
+function Button(){
+    const styles = {
+        backgroundColor:" rgb(98, 197, 222)",
+        padding: "20px",
+        fontSize: "30px",
+        border: "none",
+        borderRadius: "8px",
+}
+    return <button style={styles}>Click Me!</button>
+}
+export default Button;
